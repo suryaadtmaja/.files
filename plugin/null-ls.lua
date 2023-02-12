366939dbda94198958ac5520c639347057c7618b
sadt <suryaadtmaja13@gmail.com>
2023-02-12 21:51:27 +0700

    bla-bla-bla

--- plugin/null-ls.lua
+++ plugin/null-ls.lua
@@ -1,32 +1,44 @@
 local status, null_ls = pcall(require, "null-ls")
 if (not status) then return end
 
-local diagnostics = null_ls.builtins.diagnostics
-local formatting = null_ls.builtins.formatting
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
 
-null_ls.setup({
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+      filter = function(client)
+        return client.name == "null-ls"
+      end,
+      bufnr = bufnr,
+  })
+end
+
+null_ls.setup {
     sources = {
+        null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.diagnostics.eslint_d.with({
-            diagnostics_format = '[eslint]\n #{m}\n(#{c})'
+            diagnostics_format = '[eslint] #{m}\n(#{c})'
         }),
-        null_ls.builtins.diagnostics.fish,
-        formatting.eslint_d
+        null_ls.builtins.diagnostics.fish
     },
     on_attach = function(client, bufnr)
-      if client.server_capabilities.documentFormattingProvider then
-        vim.api.nvim_clear_autocmds { buffer = bufnr, group = augroup_format }
+      if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
         vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup_format,
-            -- buffer = 0,
+            group = augroup,
+            buffer = bufnr,
             callback = function()
-              vim.lsp.buf.format()
-            end
+              lsp_formatting(bufnr)
+            end,
         })
       end
-    end,
-})
+    end
+}
 
--- Auto commands
-vim.cmd([[ autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync() ]])
+vim.api.nvim_create_user_command(
+    'DisableLspFormatting',
+    function()
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
+    end,
+    { nargs = 0 }
+)
+
